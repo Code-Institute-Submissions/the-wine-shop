e7e4309225b0ae4b8f60e184f899e4802e893a70
Clare <clare.young07@gmail.com>
2020-10-28 17:35:14 +0000

    Created all products page and individual product page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ Return all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ Return individual product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
