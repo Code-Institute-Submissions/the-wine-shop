dfd94c98e967eb0008b087ec6602e7bcc449d7a5
Clare <clare.young07@gmail.com>
2020-10-29 22:15:33 +0000

    Shopping bag page template setup

--- shoppingbag/contexts.py
+++ shoppingbag/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from products.models import Product
+from django.shortcuts import get_object_or_404
+
 
 def shoppingbag_contents(request):
 
     shoppingbag_items = []
     total = 0
     product_count = 0
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    for item_id, quantity in shoppingbag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        shoppingbag_items.append({
+            'item_id': item_id,
+            'product': product,
+            'quantity': quantity,
+        })
 
     if total > settings.SUBSCRIPTION_LEVEL and settings.SUBSCRIPTION:
         total = total - Decimal(total * (settings.SUBSCRIPTION_DISCOUNT / 100))
@@ -21,7 +35,9 @@ def shoppingbag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'subscription_discount': settings.SUBSCRIPTION_DISCOUNT,
+        'subscription_level': settings.SUBSCRIPTION_LEVEL,
         'grand_total': grand_total,
+        'subscription': settings.SUBSCRIPTION,
     }
 
     return context
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ Return the shopping bag page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_shoppingbag(request, item_id):
+    """ Add chosen products to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if item_id in list(shoppingbag.keys()):
+        shoppingbag[item_id] += quantity
+    else:
+        shoppingbag[item_id] = quantity
+
+    request.session['shoppingbag'] = shoppingbag
+    return redirect(redirect_url)
