7ea845f45720edde99c526b205866ae2827533d4
Clare <clare.young07@gmail.com>
2020-11-04 21:42:33 +0000

    Replaced subscription variables

--- thewineshop/settings.py
+++ thewineshop/settings.py
@@ -205,8 +205,10 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+SUBSCRIPTION = True
+SUBSCRIPTION_LEVEL = 20
 STANDARD_DELIVERY = 5
-
+SUBSCRIPTION_DISCOUNT = 10
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
